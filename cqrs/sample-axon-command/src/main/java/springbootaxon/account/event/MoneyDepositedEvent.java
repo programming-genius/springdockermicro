150cf4ea011e25c8eb406be99b3edc06952ff68c
programming-genius <alessandro.z923@gmail.com>
2022-02-14 22:02:49 +0100

    Pattern

---
+++ cqrs/sample-axon-command/src/main/java/springbootaxon/account/event/MoneyDepositedEvent.java
@@ -0,0 +1,19 @@
+package springbootaxon.account.event;
+
+import java.math.BigDecimal;
+
+public class MoneyDepositedEvent extends BaseEvent<String> {
+	private  BigDecimal amount;
+
+	public MoneyDepositedEvent(String id, BigDecimal amount) {
+		super(id);
+		this.amount = amount;
+	}
+
+	public MoneyDepositedEvent() {
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+}
